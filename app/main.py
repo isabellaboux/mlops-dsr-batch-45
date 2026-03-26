c698dcb94ea68a90aae6a6ff77c0eb7e6174e973
Isabella Boux <49644498+isabellaboux@users.noreply.github.com>
2026-03-26 15:53:21 +0000

    Added comments.

--- app/main.py
+++ app/main.py
@@ -8,44 +8,54 @@ import torch.nn.functional as F
 from torchvision.transforms import v2 as transforms 
 from app.model import load_model, load_transforms
 
+
+# List of class names for model predictions.
+# Must match the order used during model training.
 categories = ["freshapple", "freshbanana", "freshorange", 
               "rottenapple", "rottenbanana", "rottenorange"]
 
-## We must define a __init__.py file in the app folder to make it a package
-#from app.model import ...
-
-# We are defining a subclass of BaseModel to structure the response data (our schema)
+# Define response schema with Pydantic.
+# FastAPI uses this to validate and serialize API outputs.
 class Result(BaseModel):
     category: str
     confidence: float
     
-    
+# Create FastAPI app instance
 app = FastAPI()
 
+# Health check endpoint
 @app.get('/')
 def read_root():
     return {"Message": "API is running. Visit /docs for the Swagger API documentation."}
 
+# Prediction endpoint
 @app.post('/predict/', response_model=Result)
 async def predict(
     input_image: UploadFile = File(...),
     model: ResNet = Depends(load_model),
     transforms: transforms.Compose = Depends(load_transforms)       
-    
     ) -> Result:
     
+    # Convert bytes to PIL image and ensure RGB color mode
     image = Image.open(io.BytesIO(await input_image.read())).convert("RGB")
     
+    # Add batch dimension for model and apply preprocessing transforms (resize, normalize, etc.)
     image = transforms(image).reshape(1, 3, 224, 224) # Add batch dimension
     
-    # turn off Dropout and BatchNorm uses stats from training
+    # Set model to evaluation mode (disable dropout, use running batch-norm stats)
     model.eval()
-    # This turns off gradient tracking for inference (we don't need gradients for inference)
+
+    # Disable gradients during prediction to save memory and improve speed
     with torch.inference_mode():
         logits = model(image)
+
+         # Convert logits to probabilities
         probs = F.softmax(logits, dim=1)
+
+        # Get highest probability and index (in our categories list)
         predicted_confidence, predicted_class_idx = torch.max(probs, dim=1)
         predicted_category= categories[predicted_class_idx.item()]
     
+    # Return a structured response
     return Result(category=predicted_category, 
                   confidence=predicted_confidence.item())
